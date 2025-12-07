1e4a07f8de3fa064474333a24587e2f839e23ae8
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2025-12-07 18:12:16 +0530

    read astro docs

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+    const posts = await getCollection("blog");
+    
+    return rss({
+        title: 'Astro Learner | Blog',
+        description: 'My journey learning Astro',
+        site: context.site,
+        items: posts.map((post) => ({
+            link: `/posts/${post.id}/`,
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+        })),
+        customData: `<language>en-us</language>`
+    });
+}
